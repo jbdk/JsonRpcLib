7d678018acbc4b9ee15227a684d9bf35072a8dee
JB <fezz.dk@gmail.com>
2018-07-22 04:52:01 +0200

    Better OrTimeout()

--- JsonRpcLib/TaskExtensions.cs
+++ JsonRpcLib/TaskExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace JsonRpcLib
@@ -18,26 +19,38 @@ namespace JsonRpcLib
 
         public static async Task OrTimeout(this Task task, TimeSpan timeout)
         {
-            var completed = await Task.WhenAny(task, Task.Delay(timeout));
-            if (completed != task)
+            using (var cancel = new CancellationTokenSource())
             {
-                throw new TimeoutException();
+                var delayTask = Task.Delay(timeout, cancel.Token);
+                if (await Task.WhenAny(task, delayTask) != task)
+                {
+                    throw new TimeoutException();
+                }
+                else
+                {
+                    cancel.Cancel();
+                }
             }
-
-            await task;
         }
 
         public static Task<T> OrTimeout<T>(this Task<T> task, int milliseconds = DEFAULTTIMEOUT) => OrTimeout(task, new TimeSpan(0, 0, 0, 0, milliseconds));
 
         public static async Task<T> OrTimeout<T>(this Task<T> task, TimeSpan timeout)
         {
-            var completed = await Task.WhenAny(task, Task.Delay(timeout));
-            if (completed != task)
+            using (var cancel = new CancellationTokenSource())
             {
-                throw new TimeoutException();
-            }
+                var delayTask = Task.Delay(timeout, cancel.Token);
+                if (await Task.WhenAny(task, delayTask) != task)
+                {
+                    throw new TimeoutException();
+                }
+                else
+                {
+                    cancel.Cancel();
+                }
 
-            return await task;
+                return task.Result;
+            }
         }
     }
 }
